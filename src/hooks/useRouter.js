dbf35230b65b77ae97aab33d49f73635014f815c
jyoon <wldbszp@gmail.com>
2022-10-16 18:35:09 +0900

    Feat: useCallback 함수로 수정

--- src/hooks/useRouter.js
+++ src/hooks/useRouter.js
@@ -1,15 +1,18 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import RouterContext from "../router/RouterContext";
 
 export const useRouter = () => {
   const { location, setLocation } = useContext(RouterContext);
 
-  const push = (to) => {
-    if (location == to) return;
+  const push = useCallback(
+    (to) => {
+      if (location == to) return;
 
-    window.history.pushState(to, to, to);
-    setLocation(to);
-  };
+      window.history.pushState(to, to, to);
+      setLocation(to);
+    },
+    [location, setLocation]
+  );
 
   return { push };
 };
